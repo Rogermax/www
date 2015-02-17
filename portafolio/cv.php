050cf4cc4761bc76bd9593fa2a282bd5d5b1d297
Roger De Moreta <rogermoreta@gmail.com>
2015-02-17 23:44:29 +0100

    Ultima actu
    asdasd

--- portafolio/cv.php
+++ portafolio/cv.php
@@ -23,6 +23,9 @@ header('Content-Type: text/html; charset=UTF-8');
 	function calculateCV() {
 		if (window.innerWidth >= 600) {
 			document.body = body;
+			document.getElementsByName("ttitulo")[0].innerHTML = '<h1 lang="cat">Currículum Vitae</h1><h1 lang="es">Currículum Vitae</h1><h1 lang="en">Curriculum Vitae</h1>';
+			document.getElementsByName("ttitulo")[1].innerHTML = '<h1 lang="cat">Currículum Vitae</h1><h1 lang="es">Currículum Vitae</h1><h1 lang="en">Curriculum Vitae</h1>';
+			document.getElementsByName("ttitulo")[2].innerHTML = '<h1 lang="cat">Currículum Vitae</h1><h1 lang="es">Currículum Vitae</h1><h1 lang="en">Curriculum Vitae</h1>';
 			document.getElementById("port-nav").innerHTML = '<span lang="cat">Portafoli</span><span lang="es">Portafolio</span><span lang="en">Portfolio</span>';
 			document.getElementById("sobremi-nav").innerHTML = '<span lang="cat">Sobre mi</span><span lang="es">Sobre mi</span><span lang="en">About</span>';
 			document.getElementById("cv-nav").innerHTML = '<span lang="cat">Curriculum Vitae</span><span lang="es">Curriculum Vitae</span><span lang="en">Curriculum Vitae</span>';
@@ -31,6 +34,9 @@ header('Content-Type: text/html; charset=UTF-8');
 		}
 		else if(window.innerWidth > 480) {
 			document.body = body;
+			document.getElementsByName("ttitulo")[0].innerHTML = '<h1>CV</h1>';
+			document.getElementsByName("ttitulo")[1].innerHTML = '<h1>CV</h1>';
+			document.getElementsByName("ttitulo")[2].innerHTML = '<h1>CV</h1>';
 			document.getElementById("port-nav").innerHTML = '<span lang="cat">Portafoli</span><span lang="es">Portafolio</span><span lang="en">Portfolio</span>';
 			document.getElementById("sobremi-nav").innerHTML = '<span lang="cat">Sobre mi</span><span lang="es">Sobre mi</span><span lang="en">About</span>';
 			document.getElementById("cv-nav").innerHTML = '<span lang="cat">CV</span><span lang="es">CV</span><span lang="en">CV</span>';
@@ -39,6 +45,9 @@ header('Content-Type: text/html; charset=UTF-8');
 		}
 		else if(window.innerWidth > 360) {
 			document.body = body;
+			document.getElementsByName("ttitulo")[0].innerHTML = '<h1>CV</h1>';
+			document.getElementsByName("ttitulo")[1].innerHTML = '<h1>CV</h1>';
+			document.getElementsByName("ttitulo")[2].innerHTML = '<h1>CV</h1>';
 			document.getElementById("port-nav").innerHTML = '<img id="portafolio" src="img/home.png"/>';
 			document.getElementById("sobremi-nav").innerHTML = '<span lang="cat">Sobre mi</span><span lang="es">Sobre mi</span><span lang="en">About</span>';
 			document.getElementById("cv-nav").innerHTML = '<span lang="cat">CV</span><span lang="es">CV</span><span lang="en">CV</span>';
@@ -47,6 +56,9 @@ header('Content-Type: text/html; charset=UTF-8');
 		}
 		else if(window.innerWidth > 270) {
 			document.body = body;
+			document.getElementsByName("ttitulo")[0].innerHTML = '<h1>CV</h1>';
+			document.getElementsByName("ttitulo")[1].innerHTML = '<h1>CV</h1>';
+			document.getElementsByName("ttitulo")[2].innerHTML = '<h1>CV</h1>';
 			document.getElementById("port-nav").innerHTML = '<img id="portafolio" src="img/home.png"/>';
 			document.getElementById("sobremi-nav").innerHTML = '<span lang="cat">Sobre mi</span><span lang="es">Sobre mi</span><span lang="en">About</span>';
 			document.getElementById("cv-nav").innerHTML = '<span lang="cat">CV</span><span lang="es">CV</span><span lang="en">CV</span>';
@@ -79,6 +91,10 @@ header('Content-Type: text/html; charset=UTF-8');
 	}
 	body.cat :lang(es), body.cat :lang(en) {
 		display: none
+	}
+
+	#table-cv {
+		border-spacing: 20px;
 	}
 
 	#titulo {
@@ -133,15 +149,12 @@ header('Content-Type: text/html; charset=UTF-8');
 	}
 
 	#main-container {    
-		border-radius: 10px;
-    	-moz-border-radius: 10px;
-    	-khtml-border-radius: 10px;
-    	-webkit-border-radius: 10px;
-    	box-shadow: 0px 0px 6px 1px #000000;
 		position: relative;
-		text-align: center;
+		text-align: left;
 		margin: 0 auto;
 		margin-top: 4px;
+		background-image: none;
+		background-color: #f1f1f1;
 		z-index: 1
 	}
 	nav ul {
@@ -192,113 +205,13 @@ header('Content-Type: text/html; charset=UTF-8');
 <title>Roger De Moreta | Mathematician and Computer Engineer</title>
 </head>
 <body class="es">
-	<?php
-	function getBrowser() 
-	{ 
-	    $u_agent = $_SERVER['HTTP_USER_AGENT']; 
-	    $bname = 'Unknown';
-	    $platform = 'Unknown';
-	    $ub = 'Unknown';
-	    $bname = 'Unknown'; 
-	    $version= "";
-
-	    //First get the platform?
-	    if (preg_match('/linux/i', $u_agent)) {
-	        $platform = 'linux';
-	    }
-	    elseif (preg_match('/macintosh|mac os x/i', $u_agent)) {
-	        $platform = 'mac';
-	    }
-	    elseif (preg_match('/windows|win32/i', $u_agent)) {
-	        $platform = 'windows';
-	    }
-	    
-	    // Next get the name of the useragent yes seperately and for good reason
-	    if(preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent)) 
-	    { 
-	        $bname = 'Internet Explorer'; 
-	        $ub = "MSIE"; 
-	    } 
-	    elseif(preg_match('/Firefox/i',$u_agent)) 
-	    { 
-	        $bname = 'Mozilla Firefox'; 
-	        $ub = "Firefox"; 
-	    } 
-	    elseif(preg_match('/Chrome/i',$u_agent)) 
-	    { 
-	        $bname = 'Google Chrome'; 
-	        $ub = "Chrome"; 
-	    } 
-	    elseif(preg_match('/Safari/i',$u_agent)) 
-	    { 
-	        $bname = 'Apple Safari'; 
-	        $ub = "Safari"; 
-	    } 
-	    elseif(preg_match('/Opera/i',$u_agent)) 
-	    { 
-	        $bname = 'Opera'; 
-	        $ub = "Opera"; 
-	    } 
-	    elseif(preg_match('/Netscape/i',$u_agent)) 
-	    { 
-	        $bname = 'Netscape'; 
-	        $ub = "Netscape"; 
-	    } 
-	    
-	    // finally get the correct version number
-	    $known = array('Version', $ub, 'other');
-	    $pattern = '#(?<browser>' . join('|', $known) .
-	    ')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
-	    if (!preg_match_all($pattern, $u_agent, $matches)) {
-	        // we have no matching number just continue
-	    }
-	    
-	    // see how many we have
-
-	    if ($matches['version']) {
-		    $i = count($matches['browser']);
-		    if ($i != 1) {
-		        //we will have two since we are not using 'other' argument yet
-		        //see if version is before or after the name
-		        if (strripos($u_agent,"Version") < strripos($u_agent,$ub)){
-		            $version= $matches['version'][0];
-		        }
-		        else {
-		            $version= $matches['version'][1];
-		        }
-		    }
-		    else {
-		    		$version= $matches['version'][0];
-		    }
-		}
-	    
-	    // check if we have a number
-	    if ($version==null || $version=="") {$version="?";}
-	    
-	    return array(
-	        'userAgent' => $u_agent,
-	        'name'      => $bname,
-	        'version'   => $version,
-	        'platform'  => $platform,
-	        'pattern'    => $pattern
-	    );
-	} 
-
-	// now try it
-	$ua=getBrowser();
-	$yourbrowser= "Your browser: " . $ua['name'] . " " . $ua['version'] . " on " .$ua['platform'];
-	//print_r($yourbrowser);
-
-	?>
-
 	<div id="header">
 	    <div id="titulo" lang="cat">
 	    	<ul>
 	    		<li id="img-roger">
 			    	<img src="img/roger.png"/>
 	    		</li>
-			    <li id="ttitulo-cat">
-			    	<h1>Portafoli</h1>
+			    <li id="ttitulo-cat" name="ttitulo">
 	    		</li>
 			    <li id="idiomas">
 			    	<div>
@@ -314,8 +227,7 @@ header('Content-Type: text/html; charset=UTF-8');
 	    		<li id="img-roger">
 			    	<img src="img/roger.png"/>
 	    		</li>
-			    <li id="ttitulo-es">
-			    	<h1>Portafolio</h1>
+			    <li id="ttitulo-es" name="ttitulo">
 	    		</li>
 			    <li id="idiomas">
 			    	<div>
@@ -331,8 +243,7 @@ header('Content-Type: text/html; charset=UTF-8');
 	    		<li id="img-roger">
 			    	<img src="img/roger.png"/>
 	    		</li>
-			    <li id="ttitulo-en">
-			    	<h1>Portfolio</h1>
+			    <li id="ttitulo-en" name="ttitulo">
 	    		</li>
 			    <li id="idiomas">
 			    	<div>
@@ -352,14 +263,45 @@ header('Content-Type: text/html; charset=UTF-8');
 			</ul>
 	    </nav>
 	</div>
-    <div id="main-container" lang="cat">  
-    	Contingut Principal.
+	<div id="main-container" lang="cat">  
+		<table id="table-cv" align="center">
+			<colgroup>
+				<col width="5%"><col width="5%">
+				<col width="5%"><col width="5%">
+				<col width="5%"><col width="5%">
+				<col width="5%"><col width="5%">
+				<col width="5%"><col width="5%">
+				<col width="5%"><col width="5%">
+				<col width="5%"><col width="5%">
+				<col width="5%"><col width="5%">
+				<col width="5%"><col width="5%">
+				<col width="5%"><col width="5%">
+			</colgroup>
+			<tr>
+				<td colspan="4"><img style="max-width: 60%; max-height: 60%" src="img/carnet.jpg"/></td>
+				<td colspan="16" id="table-nom">Roger De Moreta Salusi</td>
+			</tr>
+			<tr>
+				<td colspan="2"><span style="font-size: 30px; font-weight: 700">Estudis</span></td>
+				<td colspan="18"></td>
+			</tr>
+			<tr>
+				<td colspan="2">2011-2014</td>
+				<td colspan="18"><span style="font-weight: 700">Finalitzant l'últim curs en grau d'informática</span>, Facultat d'informàtica de Barcelona, Universitat Politècnica de Catalunya.</td>
+			</tr>
+			<tr>
+				<td colspan="2">2007-2013</td>
+				<td colspan="18"><span style="font-weight: 700">Llicenciat en matemàtiques</span>, Facultat de Matemàtiques i Estadística, Universitat Politècnica de Catalunya.</td>
+			</tr>
+		</table>
     </div>
     <div id="main-container" lang="es">  
-    	Contenido Principal.
+    	<p><img style="min-width: 100%; min-height: 100%" src="img/cv-es.png" /></p>
+    	<p><a href="pdf/RogerDeMoreta-es.pdf">Bajar versión en PDF</a></p>
     </div>
     <div id="main-container" lang="en">  
-    	Main container.
+    	<p><img style="min-width: 100%; min-height: 100%" src="img/cv-es.png" /></p>
+    	<p><a href="pdf/RogerDeMoreta-es.pdf">Download PDF version</a></p>
     </div>
     <footer lang="cat">Peu de pàgina</footer>
     <footer lang="es">Pie de página</footer>
